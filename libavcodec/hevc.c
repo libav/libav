9cbc35e9e051a07cfbef37b275bad0abbbf1e054
Anton Khirnov <anton@khirnov.net>
2013-04-28 20:07:55 +0200

    hevc: reformat pred_weight_table()

--- libavcodec/hevc.c
+++ libavcodec/hevc.c
@@ -137,7 +137,8 @@ fail:
     return AVERROR(ENOMEM);
 }
 
-static void pred_weight_table(HEVCContext *s, GetBitContext *gb) {
+static void pred_weight_table(HEVCContext *s, GetBitContext *gb)
+{
     int i = 0;
     int j = 0;
     int delta_chroma_log2_weight_denom;
@@ -147,36 +148,36 @@ static void pred_weight_table(HEVCContext *s, GetBitContext *gb) {
     uint8_t chroma_weight_l1_flag[16];
 
     s->sh.luma_log2_weight_denom = get_ue_golomb(gb);
-    if (s->sps->chroma_format_idc != 0){
+    if (s->sps->chroma_format_idc != 0) {
         delta_chroma_log2_weight_denom = get_se_golomb(gb);
         s->sh.chroma_log2_weight_denom = av_clip_c(s->sh.luma_log2_weight_denom + delta_chroma_log2_weight_denom, 0, 7);
     }
-    for ( i = 0 ; i < s->sh.num_ref_idx_l0_active ; i++){
+    for (i = 0; i < s->sh.num_ref_idx_l0_active; i++) {
         luma_weight_l0_flag[i] = get_bits1(gb);
-        if(!luma_weight_l0_flag[i]){
+        if (!luma_weight_l0_flag[i]) {
             s->sh.luma_weight_l0[i] = 1 << s->sh.luma_log2_weight_denom;
             s->sh.luma_offset_l0[i] = 0;
         }
     }
-    if( s->sps->chroma_format_idc != 0 ){ //fix me ! invert "if" and "for"
-        for( i = 0 ; i < s->sh.num_ref_idx_l0_active ; i++ ){
+    if (s->sps->chroma_format_idc != 0) { //fix me ! invert "if" and "for"
+        for (i = 0; i < s->sh.num_ref_idx_l0_active; i++) {
             chroma_weight_l0_flag[i] = get_bits1(gb);
         }
     } else {
-        for( i = 0 ; i < s->sh.num_ref_idx_l0_active ; i++ ){
+        for (i = 0; i < s->sh.num_ref_idx_l0_active; i++) {
             chroma_weight_l0_flag[i] = 0;
         }
     }
-    for( i = 0 ; i < s->sh.num_ref_idx_l0_active ; i++ ) {
-        if(luma_weight_l0_flag[i]) {
+    for (i = 0; i < s->sh.num_ref_idx_l0_active; i++) {
+        if (luma_weight_l0_flag[i]) {
             int delta_luma_weight_l0 = get_se_golomb(gb);
-            s->sh.luma_weight_l0[i] = (1 << s->sh.luma_log2_weight_denom) + delta_luma_weight_l0;
-            s->sh.luma_offset_l0[i] = get_se_golomb(gb);
+            s->sh.luma_weight_l0[i]  = (1 << s->sh.luma_log2_weight_denom) + delta_luma_weight_l0;
+            s->sh.luma_offset_l0[i]  = get_se_golomb(gb);
         }
-        if(chroma_weight_l0_flag[i]){
-            for( j = 0; j < 2; j++ ) {
-                int delta_chroma_weight_l0 = get_se_golomb(gb);
-                int delta_chroma_offset_l0 = get_se_golomb(gb);
+        if (chroma_weight_l0_flag[i]) {
+            for (j = 0; j < 2; j++) {
+                int delta_chroma_weight_l0   = get_se_golomb(gb);
+                int delta_chroma_offset_l0   = get_se_golomb(gb);
                 s->sh.chroma_weight_l0[i][j] = (1 << s->sh.chroma_log2_weight_denom) + delta_chroma_weight_l0;
                 s->sh.chroma_offset_l0[i][j] = av_clip_c((delta_chroma_offset_l0 - ((128 * s->sh.chroma_weight_l0[i][j])
                                                           >>  s->sh.chroma_log2_weight_denom) + 128), -128, 127);
@@ -187,35 +188,34 @@ static void pred_weight_table(HEVCContext *s, GetBitContext *gb) {
             s->sh.chroma_weight_l0[i][1] = 1 << s->sh.chroma_log2_weight_denom;
             s->sh.chroma_offset_l0[i][1] = 0;
         }
-
     }
-    if (s->sh.slice_type == B_SLICE){
-        for( i = 0 ; i < s->sh.num_ref_idx_l1_active ; i++ ){
+    if (s->sh.slice_type == B_SLICE) {
+        for (i = 0; i < s->sh.num_ref_idx_l1_active; i++) {
             luma_weight_l1_flag[i] = get_bits1(gb);
-            if(!luma_weight_l1_flag[i]){
+            if (!luma_weight_l1_flag[i]) {
                 s->sh.luma_weight_l1[i] = 1 << s->sh.luma_log2_weight_denom;
                 s->sh.luma_offset_l1[i] = 0;
             }
         }
-        if( s->sps->chroma_format_idc != 0 ){
-            for( i = 0 ; i < s->sh.num_ref_idx_l1_active ; i++ ){
+        if (s->sps->chroma_format_idc != 0) {
+            for (i = 0; i < s->sh.num_ref_idx_l1_active; i++) {
                 chroma_weight_l1_flag[i] = get_bits1(gb);
             }
         } else {
-            for( i = 0 ; i < s->sh.num_ref_idx_l1_active ; i++ ){
+            for (i = 0; i < s->sh.num_ref_idx_l1_active; i++) {
                 chroma_weight_l1_flag[i] = 0;
             }
         }
-        for( i = 0; i < s->sh.num_ref_idx_l1_active; i++ ) {
-            if(luma_weight_l1_flag[i] ) {
+        for (i = 0; i < s->sh.num_ref_idx_l1_active; i++) {
+            if (luma_weight_l1_flag[i]) {
                 int delta_luma_weight_l1 = get_se_golomb(gb);
-                s->sh.luma_weight_l1[i] = (1 << s->sh.luma_log2_weight_denom) + delta_luma_weight_l1;
-                s->sh.luma_offset_l1[i] = get_se_golomb(gb);
+                s->sh.luma_weight_l1[i]  = (1 << s->sh.luma_log2_weight_denom) + delta_luma_weight_l1;
+                s->sh.luma_offset_l1[i]  = get_se_golomb(gb);
             }
-            if(chroma_weight_l1_flag[i]){
-                for( j = 0; j < 2; j++ ) {
-                    int delta_chroma_weight_l1 = get_se_golomb(gb);
-                    int delta_chroma_offset_l1 = get_se_golomb(gb);
+            if (chroma_weight_l1_flag[i]) {
+                for (j = 0; j < 2; j++) {
+                    int delta_chroma_weight_l1   = get_se_golomb(gb);
+                    int delta_chroma_offset_l1   = get_se_golomb(gb);
                     s->sh.chroma_weight_l1[i][j] = (1 << s->sh.chroma_log2_weight_denom) + delta_chroma_weight_l1;
                     s->sh.chroma_offset_l1[i][j] = av_clip_c((delta_chroma_offset_l1 - ((128 * s->sh.chroma_weight_l1[i][j])
                                                               >> s->sh.chroma_log2_weight_denom) + 128), -128, 127);
@@ -226,11 +226,9 @@ static void pred_weight_table(HEVCContext *s, GetBitContext *gb) {
                 s->sh.chroma_weight_l1[i][1] = 1 << s->sh.chroma_log2_weight_denom;
                 s->sh.chroma_offset_l1[i][1] = 0;
             }
-
         }
     }
 }
-
 
 static int hls_slice_header(HEVCContext *s)
 {
