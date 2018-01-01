5e07c64443037ca1e9802e7e84412937ec937b37
Sean McGovern <gseanmcg@gmail.com>
2018-01-01 02:51:51 -0500

    cmdutils: update copyright year to 2018
    (cherry picked from commit 7b0b5bc810cdb9ef100492c9a9f2d30602c04336)
    Signed-off-by: Sean McGovern <gseanmcg@gmail.com>

--- cmdutils.c
+++ cmdutils.c
@@ -58,7 +58,7 @@
 struct SwsContext *sws_opts;
 AVDictionary *format_opts, *codec_opts, *resample_opts;
 
-static const int this_year = 2017;
+static const int this_year = 2018;
 
 void init_opts(void)
 {
